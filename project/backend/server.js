7962aa308e06da35043f63524547bfa07023727a
hossam7asan150 <141530487+hossam7asan150@users.noreply.github.com>
2023-08-17 03:26:40 +0300

    t

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
+
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+
 const port = process.env.PORT || 5001;
+
+connectDB(); // connect to MongoDB
 
 const app = express();
 
