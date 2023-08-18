45bc5b3570557a51f0f713b8c18950a7e8094c97
hossam7asan150 <141530487+hossam7asan150@users.noreply.github.com>
2023-08-18 04:10:37 +0300

    new

--- project/frontend/src/components/Header.jsx
+++ project/frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 import { Badge } from "react-bootstrap";
 const Header = () => {
    const { cartItems } = useSelector((state) => state.cart);
-   console.log(cartItems);
+   // console.log(cartItems);
    return (
       <header>
          <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
