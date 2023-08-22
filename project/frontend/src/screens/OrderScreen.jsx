ffe68f6d8daa36a2c95c93eb646cc02ae31738d9
hossam7asan150 <141530487+hossam7asan150@users.noreply.github.com>
2023-08-23 01:00:43 +0300

    new

--- project/frontend/src/screens/OrderScreen.jsx
+++ project/frontend/src/screens/OrderScreen.jsx
@@ -2,18 +2,59 @@ import { Link, useParams } from "react-router-dom";
 import { Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
+import {
+   useGetOrderDetailsQuery,
+   usePayOrderMutation,
+   useGetPayPalClientIdQuery,
+} from "../slices/ordersApiSlice";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const OrderScreen = () => {
    const { id: orderId } = useParams();
    const {
       data: order,
-      // refetch,
+      refetch,
       isLoading,
       error,
    } = useGetOrderDetailsQuery(orderId);
 
-   console.log(order);
+   // console.log(order);
+
+   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+
+   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+
+   const { userInfo } = useSelector((state) => state.auth);
+
+   const {
+      data: paypal,
+      isLoading: loadingPayPal,
+      error: errorPayPal,
+   } = useGetPayPalClientIdQuery();
+
+   useEffect(() => {
+      if (!errorPayPal && !loadingPayPal && paypal.clientId) {
+         const loadPayPalScript = async () => {
+            paypalDispatch({
+               type: "resetOptions",
+               value: {
+                  "client-id": paypal.clientId,
+                  currency: "USD",
+               },
+            });
+            paypalDispatch({ type: "setLoadingStatus", value: "pending" });
+         };
+         if (order && !order.isPaid) {
+            if (!window.paypal) {
+               loadPayPalScript();
+            }
+         }
+      }
+   }, [order, paypal, paypalDispatch, errorPayPal, loadingPayPal]);
+
    return isLoading ? (
       <Loader />
    ) : error ? (
--- project/frontend/src/slices/ordersApiSlice.js
+++ project/frontend/src/slices/ordersApiSlice.js
@@ -14,8 +14,23 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
          query: (orderId) => ({ url: `${ORDERS_URL}/${orderId}` }),
          keepUnusedDataFor: 5, // 5 seconds
       }),
+      payOrder: builder.mutation({
+         query: (orderId, paymentResult) => ({
+            url: `${ORDERS_URL}/${orderId}/pay`,
+            method: "PUT",
+            body: { ...paymentResult },
+         }),
+      }),
+      getPayPalClientId: builder.query({
+         query: () => ({ url: `${ORDERS_URL}/config/paypal` }),
+         keepUnusedDataFor: 5, // 5 seconds
+      }),
    }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery } =
-   ordersApiSlice;
+export const {
+   useCreateOrderMutation,
+   useGetOrderDetailsQuery,
+   usePayOrderMutation,
+   useGetPayPalClientIdQuery,
+} = ordersApiSlice;
